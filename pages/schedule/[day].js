6b3af884ab5f09c3b754fa02ebda4701c14e5e2e
Praditya <pradityaaldi.s@gmail.com>
2022-11-27 23:34:42 +0700

    update delete

--- pages/schedule/[day].js
+++ pages/schedule/[day].js
@@ -3,10 +3,11 @@ import NavbarLogged from "../../layout/navbar-logged";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
-export default function Details({ query }) {
+export default function Details() {
   const router = useRouter();
   const { day } = router.query;
   const [data, setData] = useState(null);
+  const [id, setId] = useState("");
 
   const getDetailSchedule = async () => {
     const email = localStorage.getItem("email");
@@ -19,6 +20,24 @@ export default function Details({ query }) {
     )
       .then((response) => response.json())
       .then((response) => setData(response.data))
+      .catch((err) => console.error(err));
+  };
+
+  const del = async () => {
+    const email = localStorage.getItem("email");
+    const options = {
+      method: "DELETE",
+    };
+
+    await fetch(
+      `https://getjadwal.api.devcode.gethired.id/schedule?email=${email}&id=${id}`,
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        document.getElementById("btnClose").click();
+        getDetailSchedule();
+      })
       .catch((err) => console.error(err));
   };
 
@@ -54,7 +73,7 @@ export default function Details({ query }) {
           <button
             className="btn btn-primary rounded-pill fs-5 fw-bold  px-4 d-flex align-items-center gap-3"
             data-bs-toggle="modal"
-            data-bs-target="#exampleModal"
+            data-bs-target="#add"
             data-cy="btn-create-schedule"
           >
             <img src="/img/plus.svg" />
@@ -62,7 +81,7 @@ export default function Details({ query }) {
           </button>
         </div>
         <hr />
-        {data != null ? (
+        {data != null && data.length > 0 ? (
           <div className="card-items py-4">
             {data.map((item, index) => {
               return (
@@ -76,7 +95,14 @@ export default function Details({ query }) {
                     <a href="javascript:void(0)">
                       <img src="/img/card-item-edit.svg" alt="edit" data-cy="card-item-edit" />
                     </a>
-                    <a href="javascript:void(0)">
+                    <a
+                      href="javascript:void(0)"
+                      data-bs-toggle="modal"
+                      data-bs-target="#delete"
+                      onClick={() => {
+                        setId(item.id);
+                      }}
+                    >
                       <img
                         src="/img/card-item-delete.svg"
                         alt="delete"
@@ -97,6 +123,52 @@ export default function Details({ query }) {
           />
         )}
       </main>
+
+      <div
+        className="modal fade"
+        id="delete"
+        tabIndex="-1"
+        aria-labelledby="deleteLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content" data-cy="form-add">
+            {/* <div className="modal-header">
+              <h5 className="modal-title fw-bold" id="deleteeLabel">
+                Buat Jadwal Kuliah
+              </h5>
+              <button type="button" data-cy="close-modal"></button>
+            </div> */}
+            <div className="modal-body text-center pt-5 px-5">
+              <img className="img-trash mb-4" src="/img/trash-illus.svg" alt="trash" />
+              <h4 className="fw-bold mb-2">Hapus Mata Kuliah</h4>
+              <p className="text-secondary-2">
+                Apakah anda yakin menghapus mata kuliah Statistika?
+              </p>
+            </div>
+            <div className="modal-footer border-0 d-flex justify-content-center gap-3">
+              <button
+                type="button"
+                className="btn btn-secondary rounded-pill fw-bold"
+                data-cy="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+                id="btnClose"
+              >
+                Batal
+              </button>
+              <button
+                type="button"
+                className="btn btn-danger rounded-pill fw-bold"
+                data-cy="btn-submit"
+                onClick={() => del()}
+              >
+                Submit
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
